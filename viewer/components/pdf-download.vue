48dac6e5905e61b7742cf598c9f2e8d860d086b8
Marcel <Schmidt>
2023-12-19 10:38:33 +0100

    feat: Pdf preview in viewer

--- viewer/components/pdf-download.vue
+++ viewer/components/pdf-download.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="pdf-download">
+  <div class="pdf-container">
     <span>{{ name }}</span>
     <VPButton :href="url" text="Download" theme="alt" />
+    <div class="pdf-container-preview">
+      <iframe :src="url" />
+    </div>
   </div>
 </template>
 
@@ -15,7 +18,7 @@ defineProps<{
 </script>
 
 <style>
-.pdf-download {
+.pdf-container {
   margin: 1rem 0;
   display: flex;
   flex-wrap: wrap;
@@ -26,7 +29,20 @@ defineProps<{
   padding: 1rem;
 }
 
-.pdf-download > * {
+.pdf-container > * {
   margin: 0.5rem;
+}
+
+.pdf-container-preview {
+  width: 100%;
+  height: auto;
+  max-height: 80vh;
+  aspect-ratio: 1 / 1.414;
+}
+
+iframe {
+  border: none;
+  width: 100%;
+  height: 100%;
 }
 </style>
